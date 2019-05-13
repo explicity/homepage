467f09d3afdc3ce96df2dd8db0515912b06867e0
Olya Yakymovych <msolya@outlook.com>
2019-05-13 21:39:04 +0300

    add .gitignore

---
+++ sw.js
@@ -0,0 +1,41 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "assets/images/logo.svg",
+    "revision": "302739ba0bb2ea96af5f1dd2399aa9e3"
+  },
+  {
+    "url": "assets/sounds/dracarys.mp3",
+    "revision": "3f75c72147477ad91d6fdd8b5e6b6f17"
+  },
+  {
+    "url": "index.html",
+    "revision": "d2e8a3ec00b07da57aeb480b90b195e6"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
